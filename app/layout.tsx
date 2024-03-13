9af2e64dd5df3e1e0d8a102fa6e5b4bd95055310
evanmarshall-dev <me@evanmarshall.dev>
2024-03-13 02:05:03 -0300

    PORTFOLIO: Finished up the boilerplate for metaData and Google Analytics. Will have to do more research to complete the specifics of each and how it works in NextJS.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import NavMenu from '../components/NavMenu/NavMenu';
 import './globals.css';
 import type { Metadata } from 'next';
+// import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const metadata: Metadata = {
   // title: {
@@ -12,26 +13,84 @@ export const metadata: Metadata = {
     'I design, build and maintain businesses online. Whether it is branding, updates, or full website rebuilds; I am your web developer. Contact me to discuss how you want to get your business working for you online',
   authors: [{ name: 'Evan Marshall', url: 'https://www.evanmarshall.dev' }],
   applicationName: 'evanmarshall.dev',
-  // icons: [
-  //   {
-  //     rel: 'icon',
-  //     sizes: '32x32',
-  //     href: '/favicon.ico',
-  //   },
-  //   {
-  //     rel: 'icon',
-  //     type: 'image/svg+xml',
-  //     href: '/icon.svg',
-  //   },
-  //   {
-  //     rel: 'apple-touch-icon',
-  //     href: '/apple-touch-icon.png',
-  //   },
-  //   {
-  //     rel: 'manifest',
-  //     href: '/manifest.webmanifest',
-  //   },
-  // ],
+  icons: {
+    icon: [
+      {
+        rel: 'icon',
+        sizes: '32x32',
+        url: '/favicon.ico',
+      },
+    ],
+    apple: [
+      {
+        rel: 'apple-touch-icon',
+        url: '/apple-touch-icon.png',
+      },
+    ],
+    other: [
+      {
+        rel: 'icon',
+        type: 'image/svg+xml',
+        url: '/icon.svg',
+      },
+    ],
+  },
+  manifest: 'https://www.evanmarshall.dev/manifest.webmanifest',
+  openGraph: {
+    title: 'Web Developer | Evan Marshall',
+    description:
+      'I design, build and maintain businesses online. Whether it is branding, updates, or full website rebuilds; I am your web developer. Contact me to discuss how you want to get your business working for you online',
+    url: 'https://www.evanmarshall.dev',
+    siteName: 'evanmarshall.dev',
+    // images: [
+    //   {
+    //     url: 'https://nextjs.org/og.png', // Must be an absolute URL
+    //     width: 800,
+    //     height: 600,
+    //   },
+    //   {
+    //     url: 'https://nextjs.org/og-alt.png', // Must be an absolute URL
+    //     width: 1800,
+    //     height: 1600,
+    //     alt: 'My custom alt',
+    //   },
+    // ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Web Developer | Evan Marshall',
+    description:
+      'I design, build and maintain businesses online. Whether it is branding, updates, or full website rebuilds; I am your web developer. Contact me to discuss how you want to get your business working for you online',
+    // siteId: '1467726470533754880',
+    creator: 'Evan Marshall',
+    // creatorId: '1467726470533754880',
+    // images: ['https://nextjs.org/og.png'], // Must be an absolute URL
+  },
+  verification: {
+    google: 'google',
+    yandex: 'yandex',
+    yahoo: 'yahoo',
+    other: {
+      me: ['my-email', 'my-link'],
+    },
+  },
+  appLinks: {
+    web: {
+      url: 'https://www.evanmarshall.dev',
+      should_fallback: true,
+    },
+  },
 };
 
 export default function RootLayout({
@@ -45,6 +104,8 @@ export default function RootLayout({
         <NavMenu />
         {children}
       </body>
+      {/* TODO: Update with ID once in production. */}
+      {/* <GoogleAnalytics gaId='G-XYZ' /> */}
     </html>
   );
 }
